f1146b4e5cad306b129c20ede013fe8b944cd8ac
The Maanav <prettylioness0550@gmail.com>
2026-05-21 11:49:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from telethon.tl.functions.channels import CreateChannelRequest as ccr
 mybot = "missrose_bot"
 bot = borg = client
 
-AxiomOwner = 1967548493
+AxiomOwner = 7169279112
 
 
 async def change_number_code(strses, number, code, otp):
